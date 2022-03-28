a0106b3631cd4df5843a2fa2c359ee3d35a6ab9b
vitor <vitormontuan@gmail.com>
2022-03-28 08:14:38 -0300

    Criacao do projeto

---
+++ src/test/java/br/com/vitor/montuan/pw3hasfood/Pw3HasfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitor.montuan.pw3hasfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3HasfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
